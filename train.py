a3e9ac81e1e65d6a306a2b914cac848914048a34
root <hbc123ht@gmail.com>
2020-10-19 15:20:05 +0000

    fuck again

--- train.py
+++ train.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 #load data
 
-train_df = pd.read_csv('data/STS-B/train.tsv', sep='\t', error_bad_lines=False)
-eval_df = pd.read_csv('data/STS-B/dev.tsv', sep='\t', error_bad_lines=False)
+train_df = pd.read_csv('data/train/train.tsv', sep='\t', error_bad_lines=False)
+eval_df = pd.read_csv('data/train/dev.tsv', sep='\t', error_bad_lines=False)
 
 train_df = train_df.rename(columns={'sentence1': 'text_a', 'sentence2': 'text_b', 'score': 'labels'}).dropna()
 eval_df = eval_df.rename(columns={'sentence1': 'text_a', 'sentence2': 'text_b', 'score': 'labels'}).dropna()
