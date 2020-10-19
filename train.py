0a58940326a73922f6be81a6c0c3c8f36b9d89f2
hbc123ht <hbc123ht@gmail.com>
2020-10-19 18:40:11 +0700

    fuck

--- train.py
+++ train.py
@@ -1,7 +1,17 @@
 from simpletransformers.classification import ClassificationModel
 from scipy.stats import pearsonr, spearmanr
+import pandas as pd
 
 
+#load data
+
+train_df = pd.read_csv('data/STS-B/train.tsv', sep='\t', error_bad_lines=False)
+eval_df = pd.read_csv('data/STS-B/dev.tsv', sep='\t', error_bad_lines=False)
+
+train_df = train_df.rename(columns={'sentence1': 'text_a', 'sentence2': 'text_b', 'score': 'labels'}).dropna()
+eval_df = eval_df.rename(columns={'sentence1': 'text_a', 'sentence2': 'text_b', 'score': 'labels'}).dropna()
+
+#model
 def pearson_corr(preds, labels):
     return pearsonr(preds, labels)[0]
 
