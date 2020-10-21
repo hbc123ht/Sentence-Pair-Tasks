d728bf42b4f9b8bb9ba081b4433ed2be1a8e07db
root <hbc123ht@gmail.com>
2020-10-21 10:46:06 +0000

    update for japanese prediction

--- predict.py
+++ predict.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from simpletransformers.classification import ClassificationModel
 from scipy.stats import pearsonr, spearmanr
 import pandas as pd
@@ -10,22 +12,14 @@ def pearson_corr(preds, labels):
 def spearman_corr(preds, labels):
     return spearmanr(preds, labels)[0]
 
-#load data
-
-train_df = pd.read_csv('data/train/train.tsv', sep='\t', error_bad_lines=False)
-eval_df = pd.read_csv('data/train/dev.tsv', sep='\t', error_bad_lines=False)
-
-train_df = train_df.rename(columns={'sentence1': 'text_a', 'sentence2': 'text_b', 'score': 'labels'}).dropna()
-eval_df = eval_df.rename(columns={'sentence1': 'text_a', 'sentence2': 'text_b', 'score': 'labels'}).dropna()
-
 model = ClassificationModel('roberta', './outputs/best_model', num_labels=1)
 # predictions, raw_outputs = model.predict("I am very fine")
 a, b= model.predict(
     [
         [
-            "Who is she ?",
-            "She is a girl",
+            "起業家精神はどれほど難しいか",
+            "非常に難しいです",
         ]
     ])
 
-print(a, b)
+print(a)
