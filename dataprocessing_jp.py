03007f22c565b3b4dad15d0de9ce761ee0da3944
hbc123ht <hbc123ht@gmail.com>
2020-10-24 14:30:55 +0700

    first commit

--- dataprocessing_jp.py
+++ dataprocessing_jp.py
@@ -2,16 +2,17 @@ import pandas as pd
 import json 
 import numpy as np
 import glob
+import random
 
-data = {
+data_positive = {
     'text_a' : [],
     'text_b' : [],
     'labels' : []
 }
 #read data
-files = glob.glob('./data3/*.json')
+files = glob.glob('./data_jp/*.json')
 for file in files:
-    print(file)
+
     with open(file) as csv_file:
         csv_reader = json.load(csv_file)
         for Q_and_A in csv_reader:
@@ -19,22 +20,41 @@ for file in files:
             answers = Q_and_A['answers']
 
             # get the max upvote
-            mx = max(int(i[1]) for i in answers)
+            # mx = max(int(i[1]) for i in answers)
             
             #append data
-            if (mx > 3):
-                for answer in answers:
+            for answer in answers:
 
-                    #process data
-                    processed_answer = ''
-                    for sentence in answer[0]:
-                        processed_answer += sentence    
-                    
-                    #append data
-                    data['text_a'].append(question)
-                    data['text_b'].append(processed_answer)
-                    data['labels'].append((5 * int(answer[1]) / mx))
-df = pd.DataFrame(data=data)
+                #process data
+                processed_answer = ''
+                for sentence in answer[0]:
+                    processed_answer += sentence    
+                
+                #append POSITIVE data
+                data_positive['text_a'].append(question)
+                data_positive['text_b'].append(processed_answer)
+                data_positive['labels'].append(1)
+
+#INTIALIZE AND CREATE NEGATIVE
+
+data_negative = {
+    'text_a' : [],
+    'text_b' : [],
+    'labels' : []
+}
+
+for x in data_positive['text_a']:
+    
+   
+    #append NEGATIVE data
+    data_negative['text_a'].append(x)
+    data_negative['text_b'].append(random.choice(data_positive['text_b']))
+    data_negative['labels'].append(0)
+
+    break
+
+
+df = pd.DataFrame(data=data_positive)
 
 data_train = df.iloc[0:int(len(df)*0.8)]
 data_test = df.iloc[int(len(df)*0.8):int(len(df)*0.9)]
@@ -51,9 +71,9 @@ def write_df_to_txt(path, the_df):
     
     file.writelines(samples)
 
-write_df_to_txt('data3/train/train.tsv', data_train)
-write_df_to_txt('data3/train/text.tsv', data_test)
-write_df_to_txt('data3/train/dev.tsv', data_dev)
+write_df_to_txt('data/train/train.tsv', data_train)
+write_df_to_txt('data/train/text.tsv', data_test)
+write_df_to_txt('data/train/dev.tsv', data_dev)
 
 # train_df = pd.read_csv('data/STS-B/train.tsv', sep='\t', error_bad_lines=False)
 # eval_df = pd.read_csv('data/STS-B/dev.tsv', sep='\t', error_bad_lines=False)
